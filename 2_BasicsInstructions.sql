d8c271c807e631a3929d312b45543e0ec95d846c
MCL - UniversDesDonnées <122817372+universdesdonnees@users.noreply.github.com>
2023-10-23 16:44:18 +0200

    Update 2_BasicsInstructions.sql

--- 2_BasicsInstructions.sql
+++ 2_BasicsInstructions.sql
@@ -1,31 +1,38 @@
 -- 1. SQL SELECT
+-- Sélectionne tous les attributs de la table Auteur
 SELECT * 
 FROM auteur;
 
 -- 2. SQL DISTINCT
+-- Permet d'obtenir les valeurs uniques des dates de naissance des auteurs
 SELECT DISTINCT date_naissance
 FROM auteur;
 
 -- 3. SQL AS (alias)
+-- Obtenir les titres des livres as le nouvel attribut 'Titre_du_Livre'
 SELECT titre AS Titre_du_Livre
 FROM livre;
 
 -- 4. SQL WHERE
+-- Obtenir les attributs de du livre Game of Thrones
 SELECT * 
 FROM livre 
 WHERE titre = 'Game of Thrones';
 
 -- 5. SQL AND & OR
+-- Obtenir les atriibuts pour  prénom George ET le nom Martin dans la table auteur 
 SELECT * 
 FROM auteur 
 WHERE prenom = 'George' AND nom = 'Martin';
 
 -- 6. SQL IN
+-- obtenir les attributs des livres 1984 et Game of thrones
 SELECT * 
 FROM livre
 WHERE titre IN ('1984', 'Game of Thrones');
 
 -- 7. SQL BETWEEN
+--Affiche les attributs des auteurs nées entre les deux dates
 SELECT * 
 FROM auteur 
 WHERE date_naissance BETWEEN '1900-01-01' AND '2000-12-31';
@@ -43,7 +50,7 @@ WHERE titre LIKE '_Game%';
 -- 8.2 SQL LIKE
 SELECT * 
 FROM livre 
-WHERE titre LIKE '_Game%';
+WHERE titre LIKE '%Game%';
 
 -- 9. SQL IS NULL / IS NOT NULL
 SELECT * 
@@ -62,16 +69,20 @@ GROUP BY genre_id
 HAVING COUNT(*) > 5;
 
 -- 12. SQL ORDER BY
+--Affiche toutes les informations des livres en rangeant les titres par ordre croissant 
 SELECT * 
 FROM livre 
 ORDER BY titre ASC;
 
 -- 13. SQL LIMIT
+-- Sélectionne toutes les informations des 5 premiers livres de la table livre
+
 SELECT * 
 FROM livre
 LIMIT 5;
 
 -- 14. SQL CASE
+-- Creation d'un attribut "Epoque" a partir d'une condtion sur la date de publication
 SELECT titre, CASE 
     WHEN date_publication < '2000-01-01' THEN '20ème siècle'
     ELSE '21ème siècle'
