6ea96449374f5cae9b00c93e1924e4ea8e350f2a
MCL - UniversDesDonnées <122817372+universdesdonnees@users.noreply.github.com>
2023-10-24 15:20:00 +0200

    Update 2_BasicsInstructions.sql

--- 2_BasicsInstructions.sql
+++ 2_BasicsInstructions.sql
@@ -1,88 +1,93 @@
 -- 1. SQL SELECT
--- Sélectionne tous les attributs de la table Auteur
+-- Cette commande permet de récupérer toutes les colonnes et toutes les lignes de la table 'auteur'.
 SELECT * 
 FROM auteur;
 
 -- 2. SQL DISTINCT
--- Permet d'obtenir les valeurs uniques des dates de naissance des auteurs
+-- Cette commande permet de récupérer des valeurs uniques pour la colonne 'date_naissance' de la table 'auteur'.
 SELECT DISTINCT date_naissance
 FROM auteur;
 
 -- 3. SQL AS (alias)
--- Obtenir les titres des livres as le nouvel attribut 'Titre_du_Livre'
+-- Cette commande renomme temporairement la colonne 'titre' en 'Titre_du_Livre' pour l'affichage des résultats.
 SELECT titre AS Titre_du_Livre
 FROM livre;
 
 -- 4. SQL WHERE
--- Obtenir les attributs de du livre Game of Thrones
+-- Cette commande récupère toutes les colonnes du livre ayant pour titre 'Game of Thrones'.
 SELECT * 
 FROM livre 
 WHERE titre = 'Game of Thrones';
 
 -- 5. SQL AND & OR
--- Obtenir les atriibuts pour  prénom George ET le nom Martin dans la table auteur 
+-- Cette commande récupère les informations de l'auteur dont le prénom est 'George' et le nom est 'Martin'.
 SELECT * 
 FROM auteur 
 WHERE prenom = 'George' AND nom = 'Martin';
 
 -- 6. SQL IN
--- obtenir les attributs des livres 1984 et Game of thrones
+-- Cette commande récupère les informations des livres ayant pour titre '1984' ou 'Game of Thrones'.
 SELECT * 
 FROM livre
 WHERE titre IN ('1984', 'Game of Thrones');
 
 -- 7. SQL BETWEEN
---Affiche les attributs des auteurs nées entre les deux dates
+-- Cette commande récupère les informations des auteurs nés entre le 1er janvier 1900 et le 31 décembre 2000.
 SELECT * 
 FROM auteur 
 WHERE date_naissance BETWEEN '1900-01-01' AND '2000-12-31';
 
 -- 8. SQL LIKE
+-- Cette commande récupère les livres dont le titre se termine par 'Game'.
 SELECT * 
 FROM livre 
 WHERE titre LIKE '%Game';
 
 -- 8.1 SQL LIKE
+-- Cette commande récupère les livres dont le titre a un caractère avant 'Game' suivi de n'importe quel caractère.
 SELECT * 
 FROM livre 
 WHERE titre LIKE '_Game%';
 
 -- 8.2 SQL LIKE
+-- Cette commande récupère les livres dont le titre contient le mot 'Game'.
 SELECT * 
 FROM livre 
 WHERE titre LIKE '%Game%';
 
 -- 9. SQL IS NULL / IS NOT NULL
+-- Cette commande récupère les livres qui n'ont pas de date de publication.
 SELECT * 
 FROM livre 
 WHERE date_publication IS NULL;
 
 -- 10. SQL GROUP BY
+-- Cette commande compte le nombre de livres par genre et les regroupe par 'genre_id'.
 SELECT genre_id, COUNT(*) 
 FROM livre 
 GROUP BY genre_id;
 
 -- 11. SQL HAVING
+-- Après regroupement par 'genre_id', cette commande ne conserve que les genres ayant plus de 5 livres.
 SELECT genre_id, COUNT(*)
 FROM livre 
 GROUP BY genre_id 
 HAVING COUNT(*) > 5;
 
 -- 12. SQL ORDER BY
---Affiche toutes les informations des livres en rangeant les titres par ordre croissant 
+-- Cette commande récupère tous les livres et les trie par titre en ordre croissant.
 SELECT * 
 FROM livre 
 ORDER BY titre ASC;
 
 -- 13. SQL LIMIT
--- Sélectionne toutes les informations des 5 premiers livres de la table livre
-
+-- Cette commande récupère les informations des 5 premiers livres de la table 'livre'.
 SELECT * 
 FROM livre
 LIMIT 5;
 
 -- 14. SQL CASE
--- Creation d'un attribut "Epoque" a partir d'une condtion sur la date de publication
+-- Cette commande ajoute une nouvelle colonne 'Epoque' basée sur la date de publication des livres.
 SELECT titre, CASE 
     WHEN date_publication < '2000-01-01' THEN '20ème siècle'
     ELSE '21ème siècle'
@@ -90,6 +95,7 @@ END AS Epoque
 FROM livre;
 
 -- 15. SQL UNION
+-- Cette commande combine les résultats des titres des livres et des noms des genres en une seule liste.
 SELECT titre 
 FROM livre
 UNION
@@ -97,6 +103,7 @@ SELECT nom_genre AS titre
 FROM genre;
 
 -- 16. SQL UNION ALL
+-- Semblable à UNION, mais conserve les doublons.
 SELECT titre 
 FROM livre
 UNION ALL
@@ -104,21 +111,25 @@ SELECT nom_genre AS titre
 FROM genre;
 
 -- 17. SQL INTERSECT 
+-- Cette commande retourne les titres des livres écrits par l'auteur avec l'id 1 qui sont également de genre_id 2.
 SELECT titre FROM livre WHERE auteur_id = 1
 INTERSECT
 SELECT titre FROM livre WHERE genre_id = 2;
 
 -- 18. SQL EXCEPT 
+-- Cette commande retourne les titres des livres écrits par l'auteur avec l'id 1 mais qui ne sont pas du genre_id 2.
 SELECT titre FROM livre WHERE auteur_id = 1
 EXCEPT
 SELECT titre FROM livre WHERE genre_id = 2;
 
 -- 19. SQL ON DUPLICATE KEY UPDATE (Note: spécifique à MySQL)
+-- Insère un nouveau livre ou met à jour le titre si le 'livre_id' existe déjà.
 INSERT INTO livre (livre_id, titre) 
 VALUES (1, 'Nouveau Livre')
 ON DUPLICATE KEY UPDATE titre = 'Nouveau Livre';
 
 -- 20. SQL MERGE 
+-- Si le 'livre_id' de la source correspond à celui de la cible, il met à jour le titre, sinon, il insère un nouveau titre.
 MERGE INTO livre AS target
 USING (
   SELECT livre_id, titre 
