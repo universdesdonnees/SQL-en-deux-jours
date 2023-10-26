76febcaa5760c1a31b639ba0e4fd3f8659f38030
MCL - UniversDesDonnées <122817372+universdesdonnees@users.noreply.github.com>
2023-10-26 13:01:46 +0200

    Create exercices2.sql

---
+++ exercices2.sql
@@ -0,0 +1,11 @@
+Listez tous les livres écrits par "George Orwell".
+Quels sont les genres les plus populaires (en termes de nombre de livres) ?  Classez-les par ordre décroissant.
+Quels auteurs ont écrit au moins deux livres pour la jeunesse ?
+Quel est le genre le plus courant parmi les livres écrits par "J.K. Rowling"?
+Quels auteurs ont écrit plus d'un livre sur la "Fiction"?
+Listez les auteurs qui ont écrit à la fois de la "Fiction" et de la "Science-fiction".
+Listez tous les livres qui ont été publiés la même année que "The Great Gatsby".
+Listez tous les auteurs qui n'ont pas encore écrit de livres (basé sur les données actuelles).
+Quel est le nombre moyen de livres par auteur ?
+Quels sont les trois genres les moins courants dans la base de données?
+Pour chaque auteur, trouvez le genre dans lequel ils ont écrit le plus de livres. Si un auteur a écrit le même nombre de livres dans plusieurs genres, listez tous ces genres.
