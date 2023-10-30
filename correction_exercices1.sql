96476f50970955070d928cbde4f91507539e16c2
MCL - UniversDesDonnées <122817372+universdesdonnees@users.noreply.github.com>
2023-10-30 13:52:54 +0100

    Update correction_exercices1.sql

--- correction_exercices1.sql
+++ correction_exercices1.sql
@@ -18,37 +18,37 @@ LIMIT 1;
 -- Trouvez tous les livres qui contiennent le mot "King" dans le titre.
 SELECT titre
 FROM livre
-where titre like '%King%';
+WHERE titre LIKE '%King%';
 
 -- Nommez tous les auteurs nés avant 1900.
 SELECT prenom, nom, date_naissance
-from auteur
-where date_naissance < '1900-01-01'
-order by date_naissance;
+FROM auteur
+WHERE date_naissance < '1900-01-01'
+ORDER BY date_naissance;
 
 -- Quel est le nombre de livres publiés chaque année ? 
 -- Classez les résultats par année de publication. 
 -- Aide : EXTRACT(YEAR FROM date_publication) AS year
-select EXTRACT(YEAR FROM date_publication) AS year, COUNT(*) as nombre_livre
+SELECT EXTRACT(YEAR FROM date_publication) AS year, COUNT(*) AS nombre_livre
 FROM livre
 GROUP by year
 ORDER by year ;
 
 -- Trouvez tous les livres qui n'ont pas été attribués à un genre.
-select * 
-from livre
-where genre_id is NULL;
+SELECT * 
+FROM livre
+WHERE genre_id is NULL;
 
 -- Combien d'auteurs ont leur anniversaire en janvier?
-select extract(MONTH from date_naissance) as MONTH, count(*)
-from auteur
-GROUP by MONTH
-having extract(MONTH from date_naissance) = 1;
+SELECT extract(MONTH from date_naissance) as month, count(*)
+FROM auteur
+GROUP BY month
+HAVING extract(month from date_naissance) = 1;
 
 -- Listez tous les auteurs dont les noms commencent par la lettre 'M'.
 SELECT prenom, nom
-from auteur
-where nom like 'M%';
+FROM auteur
+WHERE nom LIKE 'M%';
 
 -- Quelles années ont vu la publication de plus de 2 livres ?
 select EXTRACT(YEAR FROM date_publication) AS year, COUNT(*)
