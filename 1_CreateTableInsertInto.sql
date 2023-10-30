df7cc9fa1c438ca3714b0cebb7119ae3fd1f044f
MCL - UniversDesDonnées <122817372+universdesdonnees@users.noreply.github.com>
2023-10-30 13:58:00 +0100

    Update 1_CreateTableInsertInto.sql

--- 1_CreateTableInsertInto.sql
+++ 1_CreateTableInsertInto.sql
@@ -1,3 +1,6 @@
+--
+-- BIBLIOTHEQUE 
+--
 
 -- Création de la table Genre
 CREATE TABLE Genre (
@@ -98,4 +101,55 @@ INSERT INTO Livre VALUES (26, 'The Doors of Perception', '1954-11-10', 11, 17);
 INSERT INTO Livre VALUES (27, 'Les Misérables', '1862-01-14', 1, 18);             
 INSERT INTO Livre VALUES (28, 'The Raven', '1845-01-29', 15, 19);              
 INSERT INTO Livre VALUES (29, 'One Hundred Years of Solitude', '1967-06-05', 1, 20); 
+
+
+--
+-- EMPLOYE 
+--
+
+CREATE TABLE EmployeeDemographics
+(
+	EmployeeID int, 
+	FirstName varchar(50),
+	LastName varchar(50), 
+	Age int, 
+	Gender varchar(50)
+);
+
+CREATE TABLE EmployeeSalary
+(
+
+	EmployeeID int, 
+	JobTitle varchar(50),
+	Salary int
+);
+
+
+INSERT INTO EmployeeDemographics
+VALUES
+(1001, 'Jim', 'Halpert', 30, 'Male'),
+(1002, 'Pam', 'Beasley', 30, 'Female'),
+(1003, 'Dwight', 'Schrute', 29, 'Male'),
+(1004, 'Angela', 'Martin', 31, 'Female'),
+(1005, 'Toby', 'Flenderson', 32, 'Male'),
+(1006, 'Michael', 'Scott', 35, 'Male'),
+(1007, 'Meredith', 'Palmer', 32, 'Female'),
+(1008, 'Stanley', 'Hudson', 38, 'Male'),
+(1009, 'Kevin', 'Malone', 31, 'Male');
+
+
+INSERT INTO EmployeeSalary 
+VALUES
+(1001, 'Salesman', 45000),
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant', 47000),
+(1005, 'HR', 50000),
+(1006, 'Regional Manager', 65000),
+(1007, 'Supplier Relations', 41000),
+(1008, 'Salesman', 48000),
+(1009, 'Accountant', 42000);
+
+
+
 
