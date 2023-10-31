b12b1a89fd144ffd51f27350e7a7b9022ebd9e04
MCL - UniversDesDonnées <122817372+universdesdonnees@users.noreply.github.com>
2023-10-31 16:36:41 +0100

    Update correction_exercices_2.sql

--- correction_exercices_2.sql
+++ correction_exercices_2.sql
@@ -1,22 +1,28 @@
 --	Listez tous les noms d'employés qui ont le poste "Salesman"
 -- et qui ont plus de 30 ans.
 
+SELECT employeesalary.jobtitle, employeedemographics.age, employeedemographics.firstname
+FROM employeesalary
+INNER JOIN employeedemographics
+ON employeesalary.employeeid = employeedemographics.employeeid
+WHERE employeedemographics.age > 30 and employeesalary.jobtitle = 'Salesman'
+
+  
 SELECT d.firstname, s.jobtitle, d.age
-from employeedemographics d 
+FROM employeedemographics d 
 INNER JOIN employeesalary s 
-on d.employeeid = s.employeeid
+ON d.employeeid = s.employeeid
 WHERE s.jobtitle = 'Salesman' and d.age > 30;
 
 -- 	Trouvez les employés ayant un salaire supérieur 
 -- à la moyenne, mais qui ont moins de 32 ans.
 
--- salaire, age 
-select d.firstname, d.lastname, d.age
+SELECT d.firstname, d.lastname, d.age
 FROM employeedemographics d 
 INNER JOIN employeesalary s
 ON d.employeeid = s.employeeid
-Where salary > (
-  select avg(salary) as mean_salaire
-  from employeesalary
-) and d.age < 32;
+WHERE salary > (
+  SELECT avg(salary) as mean_salaire
+  FROM employeesalary
+) AND d.age < 32;
 
