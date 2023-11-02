50e5d44e8fb84e0e59927a9527021b64cb6893e5
MCL - UniversDesDonnées <122817372+universdesdonnees@users.noreply.github.com>
2023-11-02 16:03:14 +0100

    Create exercices4.sql

---
+++ exercices4.sql
@@ -0,0 +1,21 @@
+-- Manipulation de Texte : 
+UPPER(), LOWER(), INITCAP(), SUBSTRING(), POSITION(), et CHAR_LENGTH()
+
+Écrivez une requête pour afficher le nom complet de chaque auteur en combinant leurs prénoms et noms.
+Convertissez tous les noms de genres dans la table Genre en majuscules.
+Convertissez tous les prénoms d'auteurs dans la table Auteur en minuscules.
+Assurez-vous que chaque mot dans le titre d'un livre commence par une majuscule.
+Trouvez la première lettre du prénom de chaque auteur.
+Extraire les trois premiers caractères de chaque nom de genre.
+Trouvez la position du mot "The" dans tous les titres de livres.
+Calculez la longueur de chaque titre de livre.
+
+-- Manipulation de Dates
+CURRENT_DATE, AGE(), DATE_PART(), INTERVAL, DATE_TRUNC()
+Trouvez tous les auteurs qui sont nés après 1900.
+Calculez l'âge actuel de chaque auteur en utilisant leur date_naissance. (en partant du principe qu'ils sont toujours vivant)
+Extraire le mois de naissance pour chaque auteur.
+Affichez la date à laquelle chaque livre aura 10 ans
+Group by la table Livre par mois de publication et comptez le nombre de livres publiés chaque mois.
+Affichez l'année de publication pour chaque livre.
+Pour chaque livre, calculez le nombre de jours entre la date de publication du livre et la date de naissance de l'auteur.
