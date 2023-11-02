d118c81756726ff5453f82ded7826794f6d4ec49
MCL - UniversDesDonnées <122817372+universdesdonnees@users.noreply.github.com>
2023-11-02 16:02:18 +0100

    Update corrections_exercices_3.sql

--- corrections_exercices_3.sql
+++ corrections_exercices_3.sql
@@ -1,1 +1,17 @@
 
+-- CTE -- 
+-- À l'aide d'une CTE, comptez le nombre de livres publiés chaque décennies. 
+-- Ensuite, dans la requête principale, sélectionnez seulement les décennies 
+-- où plus de 5  livres ont été publiés.
+-- Aide : (annee / 10) * 10 AS decennie
+
+WITH nb_livre_decennie AS (
+Select round((EXTRACT(Year from date_publication) / 10) ,0) * 10  as decennie, 
+	count(*) as nb_livre
+FROM livre 
+GROUP by decennie 
+having count(*) > 5
+ORDER BY decennie
+)
+SELECT *
+From nb_livre_decennie
