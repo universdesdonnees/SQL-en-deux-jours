bd6d16bbb791033111687eefe389ae14ccae06a9
MCL - UniversDesDonnées <122817372+universdesdonnees@users.noreply.github.com>
2023-11-03 18:59:32 +0100

    Update 1_CreateTableInsertInto.sql

--- 1_CreateTableInsertInto.sql
+++ 1_CreateTableInsertInto.sql
@@ -147,7 +147,7 @@ VALUES
 (1001, 'Salesman', 45000),
 (1002, 'Receptionist', 36000),
 (1003, 'Salesman', 63000),
-(1004, 'Accountant', 47000),
+(1004, 'Accountant', 36000),
 (1005, 'HR', 50000),
 (1006, 'Regional Manager', 65000),
 (1007, 'Supplier Relations', 41000),
