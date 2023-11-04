97ef680a089d7d8d59ebbb2b31658dba84c0b5dc
MCL - UniversDesDonnées <122817372+universdesdonnees@users.noreply.github.com>
2023-11-04 12:55:16 +0100

    Update corrections_exercices_3.sql

--- corrections_exercices_3.sql
+++ corrections_exercices_3.sql
@@ -26,7 +26,7 @@ LEFT JOIN livre_par_auteur
 ON  a.auteur_id = livre_par_auteur.auteur_id 
 WHERE livre_par_auteur.nombre_de_livre > 2
 
--- 3
+-- 3.1
 WITH moyenne_livre_par_auteur AS (
 	SELECT AVG(nombre_de_livre) AS moyenne_livre 
 	FROM (	SELECT auteur_id,
@@ -46,28 +46,19 @@ FROM livre_par_auteur la
 CROSS JOIN moyenne_livre_par_auteur AS mla
 WHERE la.nb_livre > mla.moyenne_livre
 
-	
-with moyenne_livre_par_auteur AS (
-	select AVG(nombre_de_livre) as moyenne_livre 
-	from (
-      select auteur_id, 
-      count(*) as nombre_de_livre 
-      from livre 
-      GROUP by auteur_id)
-) 
-SELECT auteur.nom 
-from (
-      select auteur_id, 
-      count(*) as nombre_de_livre 
-      from livre 
-      GROUP by auteur_id
-	) as livre_par_auteur 
-inner join auteur 
-on auteur.auteur_id = livre_par_auteur.auteur_id
-where nombre_de_livre > (
-  select moyenne_livre
-  from moyenne_livre_par_auteur
+-- 3.2	
+WITH livre_par_auteur as (
+	SELECT l.auteur_id, au.prenom, au.nom, COUNT(*) AS nb_livre
+  	FROM livre AS l 
+  	INNER join auteur AS au 
+  	ON l.auteur_id = au.auteur_id
+  	GROUP BY l.auteur_id,  au.prenom, au.nom
 )
+SELECT nom, prenom
+FROM livre_par_auteur 
+WHERE nb_livre > (
+  SELECT AVG(nb_livre) 
+  FROM livre_par_auteur) 
 
 
 
